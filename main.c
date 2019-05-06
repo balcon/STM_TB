7b243ad4da6ba747862166c3e9b1bac6fce55dfe
Konst B <balcon@inbox.ru>
2019-05-07 00:52:08 +0300

    add init led

--- main.c
+++ main.c
@@ -35,15 +35,19 @@
 #define SW_DOWN_PORT GPIOC
 #define SW_DOWN_PIN GPIO_PIN_7
 
+#define LED_PORT GPIOB
+#define LED_PIN GPIO_PIN_5
+
 #define OPEN_TIME 300
 #define ACTIVE_DISTANCE 25
 #define CAP_MASS_TIME 50
 
 unsigned int distance=0;
 unsigned int delay=0;
+unsigned int initDelay=3000; //60sec delay for start LED
 
 void tim1Init(){ //TIM1 generate PWM with 10uS pulse per 60mS for HC-SR06 trigger
-  TIM1_TimeBaseInit(1,TIM1_COUNTERMODE_UP,70000,0);
+  TIM1_TimeBaseInit(1,TIM1_COUNTERMODE_UP,60000,0);
   TIM1_OC4Init(TIM1_OCMODE_PWM2, TIM1_OUTPUTSTATE_ENABLE, 11, TIM1_OCPOLARITY_LOW, TIM1_OCIDLESTATE_SET);
   TIM1_Cmd(ENABLE);
   TIM1_CtrlPWMOutputs(ENABLE);
@@ -62,6 +66,7 @@ void gpioInit(){
   GPIO_Init(SERVO_PORT,SERVO_PIN,GPIO_MODE_OUT_PP_LOW_SLOW);
   GPIO_Init(SW_UP_PORT,SW_UP_PIN,GPIO_MODE_IN_PU_NO_IT);
   GPIO_Init(SW_DOWN_PORT,SW_DOWN_PIN,GPIO_MODE_IN_PU_NO_IT);
+  GPIO_Init(LED_PORT,LED_PIN,GPIO_MODE_OUT_PP_LOW_SLOW);
 }
 
 void interruptConfig(){
@@ -84,23 +89,23 @@ void moveServo(){
 void main() {
   
   //CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
+  gpioInit();
   tim1Init();
   tim2Init();
-  gpioInit();
   interruptConfig();
   while (1) {
     
   } 
 }
 
-char distanceCntr=10;
+char distanceCntr=5;
 INTERRUPT_HANDLER(ECHO_IRQ_NAME, ECHO_IRQ_NUM)
 {
   unsigned int newDistance=0;
   newDistance=(TIM1_GetCounter()-550)/58;
   if(newDistance>11) distance=newDistance; 
   if(distance<ACTIVE_DISTANCE) distanceCntr--;
-    else distanceCntr=10;
+    else distanceCntr=5;
     if(!distanceCntr){
       if(!delay) delay=OPEN_TIME; //TODO buttons
       if(delay<OPEN_TIME-CAP_MASS_TIME) delay=OPEN_TIME-CAP_MASS_TIME;
@@ -108,6 +113,8 @@ INTERRUPT_HANDLER(ECHO_IRQ_NAME, ECHO_IRQ_NUM)
 }
 
 INTERRUPT_HANDLER(TIM2_UPD_OVF_BRK_IRQHandler, 13){
+  if(initDelay)
+    if(!--initDelay) GPIO_WriteHigh(LED_PORT,LED_PIN);
   if(delay){
     moveServo();
     delay--;
